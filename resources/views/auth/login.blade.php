c754ffe2116a3f1e833c843139e0e1c29717bcf4
unknown <aovanquang@gmail.com>
2020-07-15 10:40:30 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,6 +45,13 @@
                 </div>
                 {{csrf_field()}}
             </form>
+            @if(session('error'))
+                <div class="alert alert-danger" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                    {{session('error')}}
+                </div>
+            @endif
             <br>
             <p class="mb-1">
                 <a href="{{route('register')}}">Bạn chưa có tài khoản?</a>
